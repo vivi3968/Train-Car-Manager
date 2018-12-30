22ace28778f84ab2e6bec8c5c96f09f32e5b8217
vivi3968 <46255198+vivi3968@users.noreply.github.com>
2018-12-30 00:27:00 -0500

    Update TrainCar.java

--- TrainCar.java
+++ TrainCar.java
@@ -1,10 +1,3 @@
-/* Vivian Lam
- * ID: 111549991
- * vivian.lam@stonybrook.edu
- * Homework 2
- * CSE214, R11 (Reed Gantz)
- */ 
-
 /**
  *  TrainCar which contains a length in meters, a weight in tons, and a load reference (ProductLoad). 
  *  The load variable of the train may be null, which indicates that the train car is empty and contains no product.
@@ -15,16 +8,11 @@ public class TrainCar extends ProductLoad {
 	private double carWeight;
 	private ProductLoad load;
 	
-	/**
-	 * Constructor for TrainCar
-	 */
 	public TrainCar() {
 		
 	}
 	
 	/**
-	 * Constructor for TrainCar with parameters
-	 * 
 	 * @param carLength length in meters
 	 * @param carWeight weight in tons
 	 */
@@ -34,7 +22,6 @@ public class TrainCar extends ProductLoad {
 	}
 	
 	/**
-	 * Accessor method for car length
 	 * @return length of car
 	 */
 	public double getCarLength() {
@@ -42,7 +29,6 @@ public class TrainCar extends ProductLoad {
 	}
 	
 	/**
-	 * Accessor method for car weight
 	 * @return weight of car
 	 */
 	public double getCarWeight() {
@@ -50,7 +36,6 @@ public class TrainCar extends ProductLoad {
 	}
 	
 	/**
-	 * Accessor method for ProductLoad
 	 * @return ProductLoad
 	 */
 	public ProductLoad getProductLoad() {
@@ -58,7 +43,6 @@ public class TrainCar extends ProductLoad {
 	}
 	
 	/**
-	 * Mutator method for ProductLoad
 	 * @param load 
 	 */
 	public void setProductLoad(ProductLoad load) {
